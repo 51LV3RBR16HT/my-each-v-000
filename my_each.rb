f4cb064d2c2f438f3e911d0ce0a72cdfa52de401
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-09 21:09:15 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 def my_each(array) # put argument(s) here
 
-    1 = 0
-    while 1 < array.length
-      yield(array|i|)
-      i = 1 + 1
-    end
-    array
-  else
-    puts "oops"
-  end
+  #   1 = 0
+  #   while 1 < array.length
+  #     yield(array|i|)
+  #     i = 1 + 1
+  #   end
+  #   array
+  # else
+  #   puts "oops"
+  # end
 end
