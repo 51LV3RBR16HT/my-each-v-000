751802577ed06740c202a577df4ee194fb3e7567
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-09 22:46:25 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,10 @@
 def my_each(array) # put argument(s) here
   if block_given?
     i = 0
-
     while i < array.length
       yield(array[i])
       i = i + 1
     end
-
     array
   end
 end
-
-
-# my_each calls on while
-# my_each iterates over each element
-# my_each returned array contains the same elements as the original collection
-# my_each block is run n times (4)
