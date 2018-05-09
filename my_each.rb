1e90f478b7effd2d987d91cfbac318aac3639eb7
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-09 22:37:28 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,7 +3,7 @@ def my_each(array) # put argument(s) here
 end
 
 
-# use while
-# iterate over each element of my_each
+# my_each calls on while
+# my_each iterates over each element
 # my_each returned array contains the same elements as the original collection
 # my_each block is run n times (4)
